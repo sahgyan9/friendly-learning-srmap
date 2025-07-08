bcd6e1c3b2162ae10220b18c9eabf858aa752c74
sahgyan9 <152136617+sahgyan9@users.noreply.github.com>
2025-07-08 00:33:04 +0000

    Navbar on home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-
 import Hero from "@/components/Hero";
 import MentorsSection from "@/components/MentorsSection";
 import WhyFriendlyLearning from "@/components/WhyFriendlyLearning";
@@ -6,11 +5,13 @@ import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
 import { CommunityPostsSection } from "@/components/community/CommunityPostsSection";
 import SEOHead from "@/components/SEOHead";
+import Navbar from "@/components/Navbar";
 
 const Index = () => {
   return (
     <>
       <SEOHead />
+      <Navbar />
       <div className="min-h-screen flex flex-col">
         <Hero />
         <CommunityPostsSection />
